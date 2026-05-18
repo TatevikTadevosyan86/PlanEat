65fb6fc1490b23bd0e0d1b9e3934da21de8b5e7a
Tatevik Tadevosyan <tt222yi@student.lnu.se>
2026-05-18 22:10:59 +0200

    Add JWT and bcrypt imports

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/register', async (req, res, next) => {
